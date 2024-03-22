3f09af11ee0d4c15827c4250e07b838f0603ced0
Jack Qian <jackqiangc@gmail.com>
2024-03-23 00:25:05 +0800

    refactor:reconstructed boundary conditions

--- src/core/query-url.ts
+++ src/core/query-url.ts
@@ -1,6 +1,6 @@
 import { RequiredStatus } from '../constant';
 import type { JSONSchema4 } from 'json-schema';
-import type { Api, ParamDocs, RequestPath, RequestQuery } from '../types';
+import type { Api, ApiWithPathVariables, ApiWithQueryParameters, ParamDocs, RequestPath, RequestQuery } from '../types';
 
 class QueryURL {
   url: string;
@@ -9,53 +9,56 @@ class QueryURL {
 
   constructor(api: Api, baseUrl: string) {
     this.url = this.genURL(api, baseUrl);
-    this.queryParams = this.genQueryParams(api.req_query);
-    this.pathParams = this.genPathParams(api.req_params);
+    this.queryParams = this.genQueryParams(api);
+    this.pathParams = this.genPathParams(api);
   }
 
   private genURL({ path }: Api, prefix: string): string {
     return `${prefix}${path}`;
   }
 
-  private generator(params: Api['req_query'] | Api['req_params']) {
-    if (params.length) {
-      const docs: ParamDocs['docs'] = [];
-      const required: JSONSchema4['required'] = [];
-      const properties = {} as Required<JSONSchema4>['properties'];
-      params.forEach((i) => {
-        const { name, desc = '' } = i;
-        docs.push({ name, desc });
-        properties[name] = {
-          type: 'string',
-          description: desc,
-        };
-        if ('required' in i) {
-          // current params type is req_query
-          if (i.required === RequiredStatus.true) {
-            required.push(name);
-          }
-        } else {
-          // current params type is req_params
-          // all path params is required
+  private generator(
+    params: ApiWithPathVariables['req_params'] | ApiWithQueryParameters['req_query'],
+  ): RequestQuery | undefined {
+    const docs: ParamDocs['docs'] = [];
+    const required: JSONSchema4['required'] = [];
+    const properties = {} as Required<JSONSchema4>['properties'];
+    params.forEach((i) => {
+      const { name, desc = '' } = i;
+      docs.push({ name, desc });
+      properties[name] = {
+        type: 'string',
+        description: desc,
+      };
+      if ('required' in i) {
+        // current params type is req_query
+        if (i.required === RequiredStatus.true) {
           required.push(name);
         }
-      });
-      return {
-        type: 'object',
-        properties,
-        required,
-        docs,
-      };
+      } else {
+        // current params type is req_params
+        // all path params is required
+        required.push(name);
+      }
+    });
+    return {
+      type: 'object',
+      properties,
+      required,
+      docs,
+    };
+  }
+
+  private genQueryParams(api: Api): QueryURL['queryParams'] {
+    if ('req_query' in api && api.req_query.length) {
+      return this.generator(api.req_query);
     }
-    return void 0;
   }
 
-  private genQueryParams(params: Api['req_query']): QueryURL['queryParams'] {
-    return this.generator(params);
-  }
-
-  private genPathParams(params: Api['req_params']): QueryURL['pathParams'] {
-    return this.generator(params);
+  private genPathParams(api: Api): QueryURL['pathParams'] {
+    if ('req_params' in api && api.req_params.length) {
+      return this.generator(api.req_params);
+    }
   }
 }
 
